24fba593c62ebd13377456200860715b1e0a289e
Lubomir Host <lubomir.host@gmail.com>
2019-02-03 21:30:32 +0100

    Allow configure listening ip and port.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
 import webdriver
+import configparser
 
-webdriver.app.run()
+CONFIG_FNAME = 'driver.ini'
+
+config = configparser.ConfigParser()
+config.read([CONFIG_FNAME])
+
+#webdriver.app.debug = True
+webdriver.app.run(
+        host = config['webdriver']['listen'],
+        port = config['webdriver']['port']
+        )
